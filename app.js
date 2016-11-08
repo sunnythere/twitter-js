de380addd7a92ce6f537f1c3a96c21d368a4dddb
Ya-wen Alice <yawenalice@gmail.com>
2016-11-08 14:06:47 -0500

    testing

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const express = require( 'express');
+const chalk = require('chalk');
 const app = express(); // crates an instance of an express application
 const port = 3000;
 
 
 
+
 app.listen(port, function() {
-	console.log("server listening");
+	console.log(chalk.blue.italic("server listening"));
 });
 
+app.all('/', function (req, res, next) {
+	console.log(chalk.green(req.method));
+	next();
+	console.log(`good ${req.method}!`);
+})
+
 app.get('/', function(request, response) {
-	response.send('ohai')
+	response.send('hello');
+});
+
+app.get('/bears', function(request, response) {
+	response.send('Sorry, there are no bears here.');
 });
